95757e1f5d41eb3bf653f140e35dc41ed3acfb9a
viraj458 <virajmalaka94@gmail.com>
2023-08-28 15:08:58 +0530

    resolver and schema

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 
-const server = new ApolloServer();
+import { typeDefs } from "./schema.js";
+import data from "./data.js";
+
+
+const resolvers = {
+  Query: {
+    games() {
+      return data.games;
+    },
+    reviews() {
+      return data.reviews;
+    },
+    authors() {
+      return data.authors;
+    },
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
 
 const { url } = await startStandaloneServer(server, {
   listen: { port: 4000 },
